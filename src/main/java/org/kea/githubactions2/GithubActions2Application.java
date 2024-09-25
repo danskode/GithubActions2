47abe6731733670cb9b84a3b89499bed1d36e3f8
danskode <149320369+danskode@users.noreply.github.com>
2024-09-25 09:40:35 +0200

    Initial commit

---
+++ src/main/java/org/kea/githubactions2/GithubActions2Application.java
@@ -0,0 +1,13 @@
+package org.kea.githubactions2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActions2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActions2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/kea/githubactions2/GithubActions2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kea.githubactions2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActions2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
